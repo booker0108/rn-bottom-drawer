ab5f21c63c218edc6a0306be8e14a7adac870ec2
Booker Chan <booker@projectdrgn.com>
2019-05-08 13:58:29 +0800

    Add  types and fix closeing bug

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,18 @@
-declare module "rn-bottom-drawer" {}
+declare module "rn-bottom-drawer" {
+  interface Props {
+    containerHeight: number;
+    offset?: number;
+    startUp?: boolean;
+    downDisplay?: number;
+    backgroundColor?: string;
+    roundedEdges?: boolean;
+    shadow?: boolean;
+    onExpanded?: () => void;
+    onCollapsed?: () => void;
+  }
+
+  export default class BottomDrawer extends React.Component<> {
+    openDrawer: () => void;
+    closeDrawer: () => void;
+  }
+}
